811bb0074f8e2e68accab9e447f2ab16b7273feb
Hibatullah Shaalan <hshaalan@oberlin.edu>
2024-11-17 23:53:35 -0500

    pushing edited website

--- website/views.py
+++ website/views.py
@@ -1,13 +1,12 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
-
+from flask import Flask, jsonify
 views = Blueprint('views', __name__)
 #app = create_app()
 
 @views.route('/')
-@login_required
+#@login_required
 def home():
-    return render_template("home.html", user=current_user)
+    return render_template("home.html")
 
 @views.route('/about/')
 def about():
